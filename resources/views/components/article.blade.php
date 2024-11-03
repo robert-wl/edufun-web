eb630b28f33a66284cd17e135ebb5c5a9e0f9722
yahkerobertkertasnya <robert.wiliam12345@gmail.com>
2024-11-03 11:13:57 +0700

    chore: added unversioned file

---
+++ resources/views/components/article.blade.php
@@ -0,0 +1,19 @@
+<div class="card mb-3 p-5">
+    <div class="row g-5 position-relative">
+        <div class="col-md-4">
+            <img src="{{ $article->image }}" class="img-fluid rounded-start m-2 rounded-3" alt="Card Image" style="height: 100%; object-fit: cover;">
+        </div>
+        <div class="col-md-8">
+            <div class="card-body">
+                <h5 class="card-title fw-bold">{{ $article->title }}</h5>
+                <p class="card-text">
+                    <small class="text-muted">{{ $article->created_at->format('Y-m-d') }} | By {{ $article->user->name }}</small>
+                </p>
+                <p class="card-text">{{ $article->short_description }}</p>
+                <a href="{{ route('article', ['id' => $article->id]) }}">
+                    <button class="btn btn-primary position-absolute bottom-0 end-0 m-3">Read More</button>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
